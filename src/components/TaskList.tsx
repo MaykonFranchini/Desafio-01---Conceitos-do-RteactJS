c8ff02b0b925d385267df04ede59c371f7a3cd2d
MaykonFranchini <maykonfranchini2017@gmail.com>
2021-07-21 02:00:06 +0100

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const id = tasks.length + 1
+    const newTask = {
+      title: newTaskTitle,
+      id: id,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggledTasks = tasks.map(task => task.id === id ? {...task,
+    isComplete: !task.isComplete,
+      } : task )
+      setTasks(toggledTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filterTasks = tasks.filter(task => task.id !== id)
+    setTasks(filterTasks);
   }
 
   return (
